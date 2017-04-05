ea1fa87d7a3c6bc00cd855de394e18bc35a3de08
Mohamed Abdel-Hafiz <dahliamma@gmail.com>
2017-04-05 14:52:14 -0600

    added more modules, and more __init__

--- UserClass.py
+++ UserClass.py
@@ -1,12 +1,20 @@
 import xlwt
+import xlrd
+import xlutils
 class User():
     """
     User class, responsible for storing and recalling user information.
     """
     def __init__(self, FPSID):
-        self.user_name = None
+        self.user_first_name = None
+        self.user_last_name = None
         self.user_email = None
         self.user_status = None
+        self.user_strength = None
+        self.user_volume = None
         self.ID = FPSID
+        rb = xlrd.open_workbook('CoffeeUsers.xlsx')
+        wb = copy(rb).get
+
     def user_recall(self):
-        self.user_name =
+        self.user_first_name =
